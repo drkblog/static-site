cf932ada9c5e9e7b62400e4808170bf5dfbbef4c
Leandro Fernandez <drkbugs@gmail.com>
2021-12-23 00:30:38 -0300

    Show avatar

--- static/js/session.js
+++ static/js/session.js
@@ -1,7 +1,7 @@
 const greetingsSection = document.querySelector(".widget-session .greetings");
 const loginSection = document.querySelector(".widget-session .login");
 const sessionName = document.querySelector("#session-name");
-
+const avatarImage = document.querySelector("#avatar");
 
 fetch('https://tiktok-login.drkbugs.workers.dev/', {
   credentials: 'include'
@@ -13,6 +13,7 @@ fetch('https://tiktok-login.drkbugs.workers.dev/', {
     console.debug(json);
     showGreetings();
     sessionName.innerHTML = json.displayName;
+    avatarImage.src = json.avatarUrl;
 })
 .catch(err => { 
     console.debug(err);
