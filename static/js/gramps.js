ed5b48f2b3e359bd4d2c923cf42d1d5ef6425f55
Leandro <drkbugs@gmail.com>
2025-06-01 23:43:34 -0300

    Remove test button

--- static/js/gramps.js
+++ static/js/gramps.js
@@ -1,9 +1,8 @@
-// const DRK_COM_AR_GRAMPS_ENDPOINT = 'https://gramps.drkbugs.workers.dev'; 
 const DRK_COM_AR_GRAMPS_ENDPOINT = 'https://drk-com-ar-session.drkbugs.workers.dev'; 
 
 async function getGrampsSession() {
   try {
-    const response = await fetch(DRK_COM_AR_GRAMPS_ENDPOINT + '/session', { credentials: 'include' });
+    const response = await fetch(DRK_COM_AR_GRAMPS_ENDPOINT + '/gramps/person', { credentials: 'include' });
     return returnSessionIfOk(response);
   } catch (error) {
     console.error('Unable to get session:', error);
